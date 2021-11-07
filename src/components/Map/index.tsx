bc775e987bb59406feb0332775a5f16158f2baa0
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-11-07 15:52:10 +0500

    !fix: map component has been called

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -3,10 +3,12 @@
 import { FC } from 'react'
 
 export interface IMapProps {
-  MapType: google.maps.MapTypeId
+  mapType?: google.maps.MapTypeId
   mapTypeControl?: boolean
 }
 
-const Map: FC<IMapProps> = ({ MapType, mapTypeControl = false }) => <h1>hey</h1>
+const Map: FC<IMapProps> = ({ mapType, mapTypeControl = false }) => (
+  <h1>This is my map component</h1>
+)
 
 export default Map
--- src/components/Welcome/index.tsx
+++ src/components/Welcome/index.tsx
@@ -1,10 +1,12 @@
-import { FC, useEffect, useState } from 'react'
+/* eslint-disable no-undef */
+import { FC, useEffect, useRef, useState } from 'react'
+import Map from '../Map'
 import { loadMapApi } from '../../utils'
 import { Container, WelcomeText } from './index.styled'
 
 const WelcomeComponent: FC<{}> = () => {
   const [scriptloaded, setScriptLoaded] = useState<boolean>(false)
-
+  const googleRef = useRef<typeof google>()
   useEffect(() => {
     const googleMapScript = loadMapApi()
     googleMapScript.addEventListener('load', () => {
@@ -12,10 +14,21 @@ const WelcomeComponent: FC<{}> = () => {
     })
   }, [])
 
+  useEffect(() => {
+    if (scriptloaded && !googleRef.current) {
+      googleRef.current = google
+    }
+  }, [scriptloaded])
+
   return (
     <Container>
       <WelcomeText>Whatspays</WelcomeText>
-      {scriptloaded && <h1>hey</h1>}
+      {scriptloaded && (
+        <Map
+          mapType={googleRef.current?.maps.MapTypeId.ROADMAP}
+          mapTypeControl
+        />
+      )}
     </Container>
   )
 }

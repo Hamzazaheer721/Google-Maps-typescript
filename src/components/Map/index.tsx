cc715038b65d56788bdf091824602480c5a9ec35
Hamza Zaheer <62650512+Hamzazaheer721@users.noreply.github.com>
2021-11-07 16:59:01 +0500

    !fix: added map

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,14 +1,44 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
-import { FC } from 'react'
+import { FC, memo, useCallback, useRef, useState } from 'react'
+import { IGoogleMap, IGoogleLatLng } from '../../types'
 
 export interface IMapProps {
   mapType?: google.maps.MapTypeId
   mapTypeControl?: boolean
 }
 
-const Map: FC<IMapProps> = ({ mapType, mapTypeControl = false }) => (
-  <h1>This is my map component</h1>
-)
+const Map: FC<IMapProps> = memo(({ mapType, mapTypeControl = false }) => {
+  const mapRef = useRef<HTMLDivElement>(null)
+  const [map, setMap] = useState<IGoogleMap>()
+
+  const initMap = useCallback((zoomLevel: number, address: IGoogleLatLng) => {
+    if (mapRef.current) {
+      setMap(
+        new google.maps.Map(mapRef.current, {
+          zoom: zoomLevel,
+          center: address,
+          mapTypeControl,
+          streetViewControl: true,
+          zoomControl: true,
+          mapTypeId: mapType
+        })
+      )
+    }
+  }, [])
+
+  const defaultMapStart = useCallback(() => {
+    const defaultAddress = new google.maps.LatLng(65.156, 13.369)
+    initMap(5, defaultAddress)
+  }, [map])
+
+  console.log(defaultMapStart)
+
+  return (
+    <div>
+      <div ref={mapRef}>h1</div>
+    </div>
+  )
+})
 
 export default Map
---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+/* eslint-disable no-undef */
+
+export type IGoogleMap = google.maps.Map
+
+export type IGoogleLatLng = google.maps.LatLng
